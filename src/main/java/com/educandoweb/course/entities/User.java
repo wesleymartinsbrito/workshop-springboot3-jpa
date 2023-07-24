b429b1306486132f1c604441ee586aed960c3d00
Wesley Martins <wesleymartins.dev@outlook.com>
2023-07-24 19:12:17 -0300

    h2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,10 +3,17 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private Long id;
 	private String name;
 	private String email;
